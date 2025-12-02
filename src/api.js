da81935be7d062136848f0a6161e3b3ea9e77fe6
6smoke <LEON.PAVEZ@DUOCUC.CL>
2025-12-02 02:04:15 -0300

    api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
-// src/api.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://18.191.246.178:8080",
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");

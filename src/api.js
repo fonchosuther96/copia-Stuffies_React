37ea6b2a6d2f619b7e60f85b77906f8b31344b87
6smoke <LEON.PAVEZ@DUOCUC.CL>
2025-12-02 01:22:37 -0300

    .

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
+// src/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://18.191.246.178:8080", 
 });
 
-// Interceptor que agrega el token automáticamente
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // ← ESTE ES EL CORRECTO
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
